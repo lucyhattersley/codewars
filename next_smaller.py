c9f4a83d1bfe453786dd9ceed2319bc330028dcb
Lucy Hattersley <lucy.a.hattersley@gmail.com>
2019-09-06 22:17:21 +0100

    improving algorithm

--- next_smaller.py
+++ next_smaller.py
@@ -1,12 +1,28 @@
 from nose.tools import assert_equals
 
+import itertools
+
 def next_smaller(n):
-    return int("".join(sorted(str(n))))
+    numbers = []
+    
+    digits = [int(x) for x in str(n)]
+    possibles_as_set = set(itertools.permutations(digits))
+
+    for i in possibles_as_set:
+        numbers.append(int(''.join(str(x) for x in i)))
+
+    possibles = [i for i in numbers if i < n]
+        
+    if possibles != [] and len(str(max(possibles))) == len(str(n)):
+        return(max(possibles))
+    else:
+        return(-1)
+
+result = next_smaller(135)
+print(result)
 
 def test_smaller_numbers():
     assert_equals(next_smaller(907), 790)
-
-def test_further_numbers():
     assert_equals(next_smaller(531), 513)
     assert_equals(next_smaller(135), -1)
     assert_equals(next_smaller(2071), 2017)
