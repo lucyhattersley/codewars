c73452a0da67c85f975149f494187aff1dcccc77
lucyhattersley <lucy.a.hattersley@gmail.com>
2019-09-21 21:31:25 +0100

    added -1 return

--- next_smaller.py
+++ next_smaller.py
@@ -4,6 +4,9 @@ def next_smaller(n):
     digits = [int(x) for x in str(n)]
     i = 0
 
+    if all(digits[i] <= digits[i+1] for i in range(len(digits)-1)):
+        return -1
+
     while digits[-1^i] > digits[-1^i+1]:
         i = i + 1
 
@@ -22,21 +25,39 @@ def next_smaller(n):
     head[-1] = tail[current_largest]
     tail[current_largest] = temp
 
-    result = head + tail[::-1]
-    return int(''.join(map(str,result)))
+    if head[0] == 0:
+        return -1
+    else:
+        result = head + tail[::-1]
+        return int(''.join(map(str,result)))
 
 
 def test_stackexchange():
     assert_equals(next_smaller(1253479), 1249753)
 
-def test_smaller_numbers():
+def test_907():
     assert_equals(next_smaller(907), 790)
+
+
+def test_531():
     assert_equals(next_smaller(531), 513)
+
+def test_135():
     assert_equals(next_smaller(135), -1)
+
+def test_2071():
     assert_equals(next_smaller(2071), 2017)
+
+def test_414():
     assert_equals(next_smaller(414), 144)
+
+def test_123456798():
     assert_equals(next_smaller(123456798), 123456789)
+
+def test_123456789():
     assert_equals(next_smaller(123456789), -1)
+
+def test_1234567908():
     assert_equals(next_smaller(1234567908), 1234567890)
 
 """
