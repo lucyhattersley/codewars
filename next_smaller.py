a64792a6cfaba0ed0539475d2df99138289d62c4
lucyhattersley <lucy.a.hattersley@gmail.com>
2019-09-25 11:03:32 +0100

    code reworked but not functioning correctly

--- next_smaller.py
+++ next_smaller.py
@@ -14,29 +14,28 @@ def next_smaller(n):
     number = [int(x) for x in str(n)]
     n_length = len(number)  
 
-
     # Start from the right most digit and find the first 
     # digit that is greater than the digit next to it 
     for i in range(n_length-1,0,-1): 
-        if number[i] < number[i-1]: 
+        if number[i] > number[i-1]: 
             break
             
     # If no such digit found,then all numbers are in  
-    # descending order, no greater number is possible 
+    # ascending order, no lower number is possible 
     if i == 0: 
         print("Next number not possible")
         return
         
     # Find the smallest digit on the right side of  
-    # (i-1)'th digit that is greater than number[i-1] 
+    # (i-1)'th digit that is lower than number[i-1] 
     x = number[i-1] 
-    smallest = i 
+    largest = i 
     for j in range(i+1,n_length): 
-        if number[j] > x and number[j] < number[smallest]: 
-            smallest = j 
+        if number[j] < x and number[j] > number[largest]: 
+            largest = j 
         
     # Swapping the above found smallest digit with (i-1)'th 
-    number[smallest],number[i-1] = number[i-1], number[smallest] 
+    number[largest],number[i-1] = number[i-1], number[largest] 
     
     # X is the final number, in integer datatype  
     x = 0
@@ -44,8 +43,8 @@ def next_smaller(n):
     for j in range(i): 
         x = x * 10 + number[j] 
     
-    # Sort the digits after i-1 in ascending order 
-    number = sorted(number[i:]) 
+    # Sort the digits after i-1 in descending order 
+    number = sorted(number[i:], reverse=True) 
     # converting the remaining sorted digits into number 
     for j in range(n_length-i): 
         x = x * 10 + number[j] 
