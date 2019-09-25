9db4697c3c56f0d5ee134859db63133226e6178d
lucyhattersley <lucy.a.hattersley@gmail.com>
2019-09-25 10:59:55 +0100

    refactored into next_smaller

--- next_smaller.py
+++ next_smaller.py
@@ -5,47 +5,54 @@ from nose.tools import assert_equals
 # This code finds the next larger number with the same set of digits
 
 # Given number as int array, this function finds the  
-# greatest number and returns the number as integer 
-def findNext(number,n): 
-       
-     # Start from the right most digit and find the first 
-     # digit that is smaller than the digit next to it 
-     for i in range(n-1,0,-1): 
-         if number[i] > number[i-1]: 
-             break
-               
-     # If no such digit found,then all numbers are in  
-     # descending order, no greater number is possible 
-     if i == 0: 
-         print("Next number not possible")
-         return
-           
-     # Find the smallest digit on the right side of  
-     # (i-1)'th digit that is greater than number[i-1] 
-     x = number[i-1] 
-     smallest = i 
-     for j in range(i+1,n): 
-         if number[j] > x and number[j] < number[smallest]: 
-             smallest = j 
-           
-     # Swapping the above found smallest digit with (i-1)'th 
-     number[smallest],number[i-1] = number[i-1], number[smallest] 
-       
-     # X is the final number, in integer datatype  
-     x = 0
-     # Converting list upto i-1 into number 
-     for j in range(i): 
-         x = x * 10 + number[j] 
-       
-     # Sort the digits after i-1 in ascending order 
-     number = sorted(number[i:]) 
-     # converting the remaining sorted digits into number 
-     for j in range(n-i): 
-         x = x * 10 + number[j] 
-       
-     print("Next number with set of digits is",x)
+# greatest number and returns the number as integer
+# n = length of number 
+def next_smaller(n):
 
-findNext([1,2,3],3)
+    # create number and n_length from n
+
+    number = [int(x) for x in str(n)]
+    n_length = len(number)  
+
+
+    # Start from the right most digit and find the first 
+    # digit that is greater than the digit next to it 
+    for i in range(n_length-1,0,-1): 
+        if number[i] < number[i-1]: 
+            break
+            
+    # If no such digit found,then all numbers are in  
+    # descending order, no greater number is possible 
+    if i == 0: 
+        print("Next number not possible")
+        return
+        
+    # Find the smallest digit on the right side of  
+    # (i-1)'th digit that is greater than number[i-1] 
+    x = number[i-1] 
+    smallest = i 
+    for j in range(i+1,n_length): 
+        if number[j] > x and number[j] < number[smallest]: 
+            smallest = j 
+        
+    # Swapping the above found smallest digit with (i-1)'th 
+    number[smallest],number[i-1] = number[i-1], number[smallest] 
+    
+    # X is the final number, in integer datatype  
+    x = 0
+    # Converting list upto i-1 into number 
+    for j in range(i): 
+        x = x * 10 + number[j] 
+    
+    # Sort the digits after i-1 in ascending order 
+    number = sorted(number[i:]) 
+    # converting the remaining sorted digits into number 
+    for j in range(n_length-i): 
+        x = x * 10 + number[j] 
+    
+    print("Next number with set of digits is",x)
+
+next_smaller(123)
 
 
 # def next_smaller(n):
