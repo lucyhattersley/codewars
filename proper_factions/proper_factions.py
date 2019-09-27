465a6b0c17132066fea3705c97066a5b40cc2ba8
lucyhattersley <lucy.a.hattersley@gmail.com>
2019-09-27 10:44:01 +0100

    implementing euler's totient

--- proper_factions/proper_factions.py
+++ proper_factions/proper_factions.py
@@ -18,6 +18,30 @@ proper_fractions(25)==20
 Be ready to handle big numbers.
 """
 
+from fractions import Fraction
+
+from nose.tools import assert_equals
+
 def proper_fractions(n):
-    #your code here
+    #brute force method
+    count = 0)
+
+    for i in range(1,n):
+        print(Fraction(i,n))
+    print(count)
+    
+    #should return 8
+    pass
+    
+
+proper_fractions(15)
+
+def test_1():
+    assert_equals(proper_fractions(1), 0)
+
+# proper_fractions(1)==0
+# proper_fractions(2)==1
+# proper_fractions(5)==4
+# proper_fractions(15)==8
+# proper_fractions(25)==20
 
